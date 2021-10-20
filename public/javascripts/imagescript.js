524e017ed98707286b11e5f1dc6275d0925cedfe
Collin <none>
2021-10-20 03:19:23 -0700

    fix bug

--- public/javascripts/imagescript.js
+++ public/javascripts/imagescript.js
@@ -1,9 +1,17 @@
 let image = document.querySelector('#singleImage');
+scaledWidth = image.naturalWidth * 0.50;
+scaledHeight = image.naturalHeight * 0.50;
+image.width = scaledWidth;
+image.height = scaledHeight;
 
 image.addEventListener("click", (event) => {
     if ((image.getAttribute('class') === null) || (image.getAttribute('class') === "")) {
+      image.removeAttribute(width);
+      image.removeAttribute(height);
       image.setAttribute("class", "normal");
     } else {
+      image.width = scaledWidth;
+      image.height = scaledHeight;
       image.setAttribute("class", "");
     }
     event.preventDefault();
