295153d6380f34eb37389323ddb79fd9de3a7fce
Collin <none>
2021-11-03 03:01:55 -0700

    bug fix

--- public/javascripts/mainscript.js
+++ public/javascripts/mainscript.js
@@ -23,22 +23,14 @@ function updateFile() {
 
 function computeLayout() {
   const images = document.querySelectorAll(".imgContainer");
-  //console.log(images);
 
   for (let containerNode of Array.from(images)) {
-    console.log(containerNode);
     let imageNode = containerNode.childNodes[0].childNodes[0];
     imageNode.addEventListener("load", () => {
-      //console.log(imageNode);
-      //console.log(imageNode.naturalWidth);
       let scaledWidth = imageNode.naturalWidth * 0.30;
       let scaledHeight = imageNode.naturalHeight * 0.30;
-      //console.log(scaledWidth);
-      //console.log(scaledHeight);
-      //containerNode.width = scaledWidth;
-      //containerNode.height = scaledHeight;
       containerNode.setAttribute('style', `width: ${scaledWidth}px; height: ${scaledHeight}px`);
-      //console.log(containerNode);
+      containerNode.setAttribute('class', "imgContainer");
     });
   }
 }
